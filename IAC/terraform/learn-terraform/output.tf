cd647d410234791f63f0ea2c9e1de35d7d2b82c2
Dipankar Das <dipsonu10@hotmail.com>
2022-04-18 23:40:22 +0530

    Started to learn Terraform (IAC)

---
+++ IAC/terraform/learn-terraform/output.tf
@@ -0,0 +1,9 @@
+output "container_id" {
+  description = "ID of the Docker container"
+  value       = docker_container.nginx.id
+}
+
+output "image_id" {
+  description = "ID of the Docker image"
+  value       = docker_image.nginx.id
+}
---
+++ IAC/terraform/learn-terraform/variables.tf
@@ -0,0 +1,6 @@
+
+variable "nameOfContainer" {
+  description = "value of the name of me"
+  type = string
+  default = "ExampleNginxContainer"
+}
