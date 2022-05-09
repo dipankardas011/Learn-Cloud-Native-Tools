55fd4d30b59ffd91f2342a14d22270878c63f7c0
Dipankar Das <dipsonu10@hotmail.com>
2022-05-09 22:48:04 +0530

    Starting to learn Jenkins

---
+++ CI-CD/Jenkins/cmd.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker run --name jenkins-docker --rm --detach --privileged --network jenkins --network-alias docker --env DOCKER_TLS_CERTDIR=/certs --volume jenkins-docker-certs:/certs/client --volume jenkins-data:/var/jenkins_home --publish 2376:2376 docker:dind
+docker build -t jenkins .
+docker run --name jenkins-blueocean --rm --detach --network jenkins --env DOCKER_HOST=tcp://docker:2376 --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 --volume jenkins-data:/var/jenkins_home --volume jenkins-docker-certs:/certs/client:ro --publish 8080:8080 --publish 50000:50000 jenkins
