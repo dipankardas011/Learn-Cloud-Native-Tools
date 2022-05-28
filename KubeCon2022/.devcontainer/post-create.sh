d4d56a5acd786ba1ab9e470f05984036709d0d0d
Dipankar Das <dipsonu10@hotmail.com>
2022-05-28 19:24:46 +0530

    Switched to Linux
    Signed-off-by: Dipankar Das <dipsonu10@hotmail.com>

--- KubeCon2022/.devcontainer/post-create.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# this runs at Codespace creation - not part of pre-build
-
-echo "post-create start"
-echo "$(date)    post-create start" >> "$HOME/status"
-
-echo "post-create complete"
-echo "$(date +'%Y-%m-%d %H:%M:%S')    post-create complete" >> "$HOME/status"
